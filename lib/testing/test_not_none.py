fef76c05c684f40f1d29eaaa6b14e2a716b4e2c9
Brian Joe Kamau <joebrian998@gmail.com>
2024-12-07 09:30:07 +0300

    debugging with pytest

--- lib/testing/test_not_none.py
+++ lib/testing/test_not_none.py
@@ -2,6 +2,10 @@
 
 from not_none_functions import return_not_none
 
+def test_return_not_none():
+    '''in not_none_functions, function "return_not_none" does not return None.'''
+    assert return_not_none() is not None
+    
 # def test_return_not_none():
 #     '''in not_none_functions, function "return_not_none" returns a value that is not None.'''
 #     assert False
